428822400a23259c7f50365caeec0ed21d40f354
Surender Singh <surender19@hotmail.com>
2015-06-12 22:58:01 +0530

    Added AzureStorageAccessLayer,CloudService,ConfigManager,Service Components

---
+++ WordyFly/WordFly.AzureStorageAccessLayer/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordFly.AzureStorageAccessLayer
+{
+    /// <summary>
+    /// SRP of handling all the constats of AzureStorage layer
+    /// </summary>
+    class Constants
+    {
+    }
+}
---
+++ WordyFly/WordFly.AzureStorageAccessLayer/StorageAccessBase.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace WordFly.AzureStorageAccessLayer
+{
+    /// <summary>
+    /// Base class to interact with Table Storgae
+    /// </summary>
+    abstract class StorageAccessBase
+    {
+        public CloudStorageAccount StorageAcount { get; private set; }
+
+        public CloudTableClient TableClient { get; private set; }
+
+        public CloudTable EntityTable { get; private set; }
+
+        public bool IsTableLoaded { get; private set; }
+
+        public StorageAccessBase()
+        {
+            StorageAcount = CloudStorageAccount.Parse(ConfigManager.Config.TableConnectionString);
+            TableClient = StorageAcount.CreateCloudTableClient();
+        }
+        public bool LoadEntityTable(string entityType)
+        {
+            try
+            {
+                EntityTable = TableClient.GetTableReference(entityType);
+                EntityTable.CreateIfNotExists();
+                IsTableLoaded = true;
+            }
+            catch (Exception)
+            {
+                IsTableLoaded = false;
+            }
+
+            return IsTableLoaded;
+        }
+
+        public bool GetTableLoadedState(string expectedEntity)
+        {
+            bool loaded = false;
+            if (IsTableLoaded)
+            {
+                loaded = EntityTable.Name == expectedEntity;
+            }
+            return loaded;
+        }
+
+        public void LoadTable(string tableName)
+        {
+            if (!GetTableLoadedState(tableName))
+            {
+                LoadEntityTable(tableName);
+            }
+            if (!IsTableLoaded)
+                throw new FieldAccessException(string.Format("Cloud Table: {0} not loaded", tableName));
+        }
+    }
+}
